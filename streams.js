ec1cddef448109de02e0526f3bed3273fb213110
joellegg <joellegg@bellsouth.net>
2017-03-05 15:38:27 -0600

    file content appends to the end of the new file, all CAPS

--- streams.js
+++ streams.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
 const { Transform, Writable } = require('stream');
-const { createReadStream } = require('fs');
+const { createReadStream, appendFile } = require('fs');
 
 const transformStream = Transform();
 const writeStream = Writable();
-
+// const writeStream2 = Writable();
+let [,, ...fileCopy] = process.argv;
 
 // READ
 const readStream = createReadStream('languages.json');
+// const readStream2 = createReadStream(`${fileCopy}`);
+// if (readStream === readStream2) {
+//   console.log('the two reads match')
+// }
 
 // TRANSFORM
 transformStream._transform = (buffer, encoding, done) => {
@@ -17,7 +22,15 @@ transformStream._transform = (buffer, encoding, done) => {
 
 // WRITE
 writeStream._write = (buffer, encoding, done) => {
-  process.stdout.write(`${buffer}`);
+  // process.stdout.write(`${buffer}`);
+  appendFile(`${fileCopy}`, `${buffer}`, (err) => {
+    if (err) throw err;
+    console.log(`The data was appended to ${fileCopy}!`);
+  });
 };
+// writeStream2._write = (buffer) => {
+//   console.log(`${buffer.toString()}`)
+// }
 
+// readStream2.pipe(writeStream2)
 readStream.pipe(transformStream).pipe(writeStream);
