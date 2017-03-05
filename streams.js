63e77e9c6f35a7845b96492a14f6137be49b0abe
joellegg <joellegg@bellsouth.net>
2017-03-05 14:04:18 -0600

    printing file contents to console

--- streams.js
+++ streams.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log('joel! You are at it again!')
+
+const { Transform, Writable } = require('stream');
+const { createReadStream } = require('fs');
+
+const transformStream = Transform();
+const writeStream = Writable();
+
+
+// READ
+const readStream = createReadStream('languages.json');
+
+// TRANSFORM
+transformStream._transform = (buffer, encoding, done) => {
+  done(null, `${buffer.toString().toUpperCase()}`);
+};
+
+// WRITE
+writeStream._write = (buffer, encoding, done) => {
+  process.stdout.write(`${buffer}`);
+};
+
+readStream.pipe(transformStream).pipe(writeStream);
